65f7e89836dce27406f054f7a58b81c36e02ad6b
popcorny <celu@infuseai.io>
2025-02-24 09:55:17 +0800

    Update the CLL in view option
    Signed-off-by: popcorny <celu@infuseai.io>

--- js/src/components/lineage/LineageView.tsx
+++ js/src/components/lineage/LineageView.tsx
@@ -297,10 +297,6 @@ export function PrivateLineageView(
   }>({ x: 0, y: 0 });
 
   const [breakingChangeEnabled, setBreakingChangeEnabled] = useState(false);
-  const [columnLevelLinage, setColumnLevelLinage] = useState<{
-    node: string;
-    column: string;
-  }>();
 
   const toast = useToast();
 
@@ -341,7 +337,7 @@ export function PrivateLineageView(
       let [nodes, edges] = toReactflow(
         lineageGraph,
         selectedNodes,
-        columnLevelLinage
+        viewOptions.column_level_lineage
       );
       let nodeSet = selectAllNodes(lineageGraph);
       if (isModelsChanged) {
@@ -567,7 +563,7 @@ export function PrivateLineageView(
     let [newNodes, newEdges] = toReactflow(
       lineageGraph,
       selectedNodes,
-      columnLevelLinage
+      newViewOptions.column_level_lineage
     );
     const nodeSet = selectImpactRadius(lineageGraph, breakingChangeEnabled);
     [newNodes, newEdges] = highlightNodes(
@@ -933,10 +929,16 @@ export function PrivateLineageView(
 
     // Column Level Lineage
     showColumnLevelLineage: (node: string, column: string) => {
-      setColumnLevelLinage({ node, column });
+      handleViewOptionsChanged({
+        ...viewOptions,
+        column_level_lineage: { node, column },
+      });
     },
     resetColumnLevelLinage: () => {
-      setColumnLevelLinage(undefined);
+      handleViewOptionsChanged({
+        ...viewOptions,
+        column_level_lineage: undefined,
+      });
     },
   };
 
@@ -1029,11 +1031,16 @@ export function PrivateLineageView(
                     trackBreakingChange({ enabled });
                   }}
                 />
-                {columnLevelLinage && (
+                {viewOptions.column_level_lineage && (
                   <ColumnLevelLineageControl
-                    node={columnLevelLinage.node}
-                    column={columnLevelLinage.column}
-                    reset={() => setColumnLevelLinage(undefined)}
+                    node={viewOptions.column_level_lineage.node}
+                    column={viewOptions.column_level_lineage.column}
+                    reset={() => {
+                      handleViewOptionsChanged({
+                        ...viewOptions,
+                        column_level_lineage: undefined,
+                      });
+                    }}
                   ></ColumnLevelLineageControl>
                 )}
                 {nodes.length == 0 && (
